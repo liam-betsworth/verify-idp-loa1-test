2a061a2128bcddf2a787fdaf81f51a6003daec7a
Liam Betsworth <liam.betsworth@gofore.com>
2018-01-17 15:53:43 +0000

    Test

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ var nunjucksAppEnv = nunjucks.configure(appViews, {
 // Nunjucks filters
 utils.addNunjucksFilters(nunjucksAppEnv)
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Set views engine
 app.set('view engine', 'html')
 
@@ -121,13 +128,6 @@ app.use(function (req, res, next) {
   res.setHeader('X-Robots-Tag', 'noindex')
   next()
 })
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
-  next();
-});
 
 app.get('/robots.txt', function (req, res) {
   res.type('text/plain')
